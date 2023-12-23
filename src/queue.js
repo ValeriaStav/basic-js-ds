088b0ba7aad80fbc271af0db49293fab40a6f2ad
ValeriaStav <valeriiastavriadi@gmail.com>
2023-12-24 01:03:19 +0300

    feat: add queue solution

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,23 +14,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+    }
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    getUnderlyingList() {
+        return this.first;
+    }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    enqueue(value) {
+        const newNode = new ListNode(value);
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+        if (!this.last) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+    }
+
+    dequeue() {
+        if (!this.first) return null;
+
+        const itemToDel = this.first.value;
+
+        if (this.first.next) {
+            this.first = this.first.next;
+        } else {
+            this.first = null;
+            this.last = null;
+        }
+        return itemToDel;
+    }
 }
 
 module.exports = {
-  Queue
+    Queue,
 };
